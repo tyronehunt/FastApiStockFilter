bb3bd0371225ad338bf7bddc8a62b74b2ca4a10f
Tyrone Hunt <tyrone.hunt07@gmail.com>
2020-11-19 10:55:26 +0000

    sqlalchemy added with table schema and intro to pydantic

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+import models
+from database import SessionLocal, engine
+from models import Stock
+
 app = FastAPI()
 
+models.Base.metadata.create_all(bind=engine)
+
 templates = Jinja2Templates(directory="templates")
+
+class StockRequest(BaseModel):
+    symbol: str
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
 def home(request: Request):
@@ -15,10 +36,16 @@ def home(request: Request):
 
 
 @app.post("/stock")
-def create_stock():
+def create_stock(stock_request: StockRequest, db: Session = Depends(get_db)):
     """
     Creates a stock and stores in the DB
     """
+    stock = Stock()
+    stock.symbol = stock_request.symbol
+
+    db.add(stock)
+    db.commit()
+
     return {
         "code": "success",
         "message": "stock created"
---
+++ models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Numeric
+
+# If you had relationships you'd need these:
+# from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class Stock(Base):
+    __tablename__ = "stocks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    symbol = Column(String, unique=True, index=True)
+    price = Column(Numeric(10, 2))
+    forward_pe = Column(Numeric(10, 2))
+    forward_eps = Column(Numeric(10, 2))
+    dividend_yield = Column(Numeric(10, 2))
+    ma50 = Column(Numeric(10, 2))
+    ma200 = Column(Numeric(10, 2))
+
+    # If had relationships
+    # items = relationship("Item", back_populates="owner")
